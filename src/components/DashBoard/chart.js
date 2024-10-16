470604899cccc6aa881ad618553bc96a4972bcf3
sahravi63 <rs5982@srmist.edu.in>
2024-10-16 19:11:00 +0530

    first commit

---
+++ src/components/DashBoard/chart.js
@@ -0,0 +1,16 @@
+/* Add this to your CSS file */
+.dashboard-container {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    padding: 20px;
+  }
+  
+  .graph-container {
+    width: 45%;
+  }
+  
+  .gauge-container {
+    width: 45%;
+  }
+  
